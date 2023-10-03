9b6f67fa991a5dc7b1a755b412a848d420ce2a5b
Mark Kellogg <mkkellogg@gmail.com>
2023-10-03 07:10:15 -0700

    More work on sorting

--- src/SortWorker.js
+++ src/SortWorker.js
@@ -35,19 +35,11 @@ export function createSortWorker(self) {
         const centerCov = new Float32Array(workerTransferCenterCovarianceBuffer);
 
 
-        /*if (depthMix.length !== vertexCount) {
+        if (depthMix.length !== vertexCount) {
             depthMix = new BigInt64Array(vertexCount);
-        } else {
-            let dot =
-                lastProj[2] * viewProj[2] +
-                lastProj[6] * viewProj[6] +
-                lastProj[10] * viewProj[10];
-            if (Math.abs(dot - 1) < 0.01) {
-                self.postMessage({'sortCanceled': true});
-                return;
-            }
-        }
+         }
 
+        console.time("DEFAULT");
         const floatMix = new Float32Array(depthMix.buffer);
         const indexMix = new Uint32Array(depthMix.buffer);
 
@@ -61,21 +53,13 @@ export function createSortWorker(self) {
             floatMix[2 * j + 1] = dx * dx + dy * dy + dz * dz;
         }
         lastProj = viewProj;
-        depthMix.sort();*/
+        depthMix.sort();
+        console.timeEnd("DEFAULT");
 
 
+       
 
-        if (depthMix.length === vertexCount) {
-            let dot =
-                lastProj[2] * viewProj[2] +
-                lastProj[6] * viewProj[6] +
-                lastProj[10] * viewProj[10];
-            if (Math.abs(dot - 1) < 0.01) {
-                self.postMessage({'sortCanceled': true});
-                return;
-            }
-        }
-
+        /*console.time("MYCOUNT");
         if (!distances || distances.length < vertexRenderCount) {
             distances = new Uint32Array(vertexRenderCount);
         }
@@ -94,17 +78,21 @@ export function createSortWorker(self) {
         }
 
         const distancesRange = maxDistance - minDistance;
-        if (!frequencies || frequencies.length < distancesRange) {
-            frequencies = new Uint32Array(distancesRange);
-        }
+        const mappedRange = 0x1 << 16;
+        const rangeMap = mappedRange / distancesRange;
+        //if (!frequencies || frequencies.length < mappedRange) {
+            frequencies = new Uint32Array(mappedRange);
+       // }
+       // for (let i = 0; i < mappedRange; i++) frequencies[i] = 0;
+
         for (let i = 0; i < vertexRenderCount; i++) {
-            const frequenciesIndex = distances[i] - minDistance;
+            const frequenciesIndex = Math.floor((distances[i] - minDistance) * rangeMap);
             const cFreq = frequencies[frequenciesIndex] || 0;
             frequencies[frequenciesIndex] = cFreq + 1;   
         }
 
         let cumulativeFreq = 0;
-        for (let i = 0; i < distancesRange; i++) {
+        for (let i = 1; i < mappedRange; i++) {
             const cFreq = frequencies[i];
             cumulativeFreq += cFreq;
             frequencies[i] = cumulativeFreq;
@@ -113,14 +101,54 @@ export function createSortWorker(self) {
         if (!realIndex || realIndex.length < vertexRenderCount) {
             realIndex = new Uint32Array(vertexRenderCount);
         }
-        for (let i = 0; i < vertexRenderCount; i++) {
-            const frequenciesIndex = distances[i] - minDistance;
-            realIndex[frequencies[frequenciesIndex]] = i;
+
+        for (let i = vertexRenderCount - 1; i >= 0; i--) {
+            const frequenciesIndex =  Math.floor((distances[i] - minDistance) * rangeMap);
+            const freq = frequencies[frequenciesIndex];
+            realIndex[freq- 1] = indexArray[i];
+            frequencies[frequenciesIndex] = freq - 1;
         }
+        console.timeEnd("MYCOUNT");*/
+
+
+
+        /*console.time("COUNT");
+        let maxDepth = -Infinity;
+		let minDepth = Infinity;
+		let sizeList = new Int32Array(vertexRenderCount);
+		for (let i = 0; i < vertexRenderCount; i++) {
+            const splatArrayBase = rowSizeFloats * indexArray[i];
+            let depth =
+				((viewProj[2] * splatArray[splatArrayBase] +
+					viewProj[6] * splatArray[splatArrayBase + 1] +
+					viewProj[10] * splatArray[splatArrayBase + 2]) *
+					4096) |
+				0;
+			sizeList[i] = depth;
+			if (depth > maxDepth) maxDepth = depth;
+			if (depth < minDepth) minDepth = depth;
+		}
+
+		// This is a 16 bit single-pass counting sort
+		let depthInv = (256 * 256) / (maxDepth - minDepth);
+		let counts0 = new Uint32Array(256*256);
+		for (let i = 0; i < vertexRenderCount; i++) {
+			sizeList[i] = ((sizeList[i] - minDepth) * depthInv) | 0;
+			counts0[sizeList[i]]++;
+		}
+		let starts0 = new Uint32Array(256*256);
+		for (let i = 1; i < 256*256; i++) starts0[i] = starts0[i - 1] + counts0[i - 1];
+		depthIndex = new Uint32Array(vertexRenderCount);
+		for (let i = 0; i < vertexRenderCount; i++) depthIndex[starts0[sizeList[i]]++] = indexArray[i];
+
+        console.timeEnd("COUNT");*/
+
 
 
         for (let j = 0; j < vertexRenderCount; j++) {
-            const i = realIndex[j];
+            const i = indexMix[2 * j];
+          //  const i = realIndex[j];
+         //  const i = depthIndex[j];
 
             const centerCovBase = 9 * j;
             const colorBase = 4 * j;
