72e92803c13b854a6865fded9046af4e12f51175
Timo Korkalainen <timo@ctrlreality.fi>
2023-10-09 22:05:46 +0300

    Exports SplatMesh

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { PlyLoader } from './PlyLoader.js';
 import { SplatLoader } from './SplatLoader.js';
+import { SplatMesh } from './SplatMesh.js';
 import { Viewer } from './Viewer.js';
 
 export {
     PlyLoader,
     SplatLoader,
+    SplatMesh,
     Viewer
 };
