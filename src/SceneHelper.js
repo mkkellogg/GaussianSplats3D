d5e86bfe3887109c5b1ce2701daf2fb2ce430e06
Mark Kellogg <mkkellogg@gmail.com>
2023-10-18 15:31:24 -0700

    Added SceneHelper + more refactoring

---
+++ src/SceneHelper.js
@@ -0,0 +1,104 @@
+import * as THREE from 'three';
+
+export class SceneHelper {
+
+    constructor(scene) {
+        this.scene = scene;
+    }
+
+    addDebugMeshes() {
+        this.debugRoot = this.createDebugMeshes();
+        this.secondaryDebugRoot = this.createSecondaryDebugMeshes();
+        this.scene.add(this.debugRoot);
+        this.scene.add(this.secondaryDebugRoot);
+    }
+
+    createDebugMeshes(renderOrder) {
+        const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+        const debugMeshRoot = new THREE.Object3D();
+
+        const createMesh = (color, position) => {
+            let sphereMesh = new THREE.Mesh(sphereGeometry, SceneHelper.buildDebugMaterial(color));
+            sphereMesh.renderOrder = renderOrder;
+            debugMeshRoot.add(sphereMesh);
+            sphereMesh.position.fromArray(position);
+        };
+
+        createMesh(0xff0000, [-50, 0, 0]);
+        createMesh(0xff0000, [50, 0, 0]);
+        createMesh(0x00ff00, [0, 0, -50]);
+        createMesh(0x00ff00, [0, 0, 50]);
+        createMesh(0xffaa00, [5, 0, 5]);
+
+        return debugMeshRoot;
+    }
+
+    createSecondaryDebugMeshes(renderOrder) {
+        const boxGeometry = new THREE.BoxGeometry(3, 3, 3);
+        const debugMeshRoot = new THREE.Object3D();
+
+        let boxColor = 0xBBBBBB;
+        const createMesh = (position) => {
+            let boxMesh = new THREE.Mesh(boxGeometry, SceneHelper.buildDebugMaterial(boxColor));
+            boxMesh.renderOrder = renderOrder;
+            debugMeshRoot.add(boxMesh);
+            boxMesh.position.fromArray(position);
+        };
+
+        let separation = 10;
+        createMesh([-separation, 0, -separation]);
+        createMesh([-separation, 0, separation]);
+        createMesh([separation, 0, -separation]);
+        createMesh([separation, 0, separation]);
+
+        return debugMeshRoot;
+    }
+
+    static buildDebugMaterial(color) {
+        const vertexShaderSource = `
+            #include <common>
+            varying float ndcDepth;
+
+            void main() {
+                gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position.xyz, 1.0);
+                ndcDepth = gl_Position.z / gl_Position.w;
+                gl_Position.x = gl_Position.x / gl_Position.w;
+                gl_Position.y = gl_Position.y / gl_Position.w;
+                gl_Position.z = 0.0;
+                gl_Position.w = 1.0;
+    
+            }
+        `;
+
+        const fragmentShaderSource = `
+            #include <common>
+            uniform vec3 color;
+            varying float ndcDepth;
+            void main() {
+                gl_FragDepth = (ndcDepth + 1.0) / 2.0;
+                gl_FragColor = vec4(color.rgb, 0.0);
+            }
+        `;
+
+        const uniforms = {
+            'color': {
+                'type': 'v3',
+                'value': new THREE.Color(color)
+            },
+        };
+
+        const material = new THREE.ShaderMaterial({
+            uniforms: uniforms,
+            vertexShader: vertexShaderSource,
+            fragmentShader: fragmentShaderSource,
+            transparent: false,
+            depthTest: true,
+            depthWrite: true,
+            side: THREE.FrontSide
+        });
+        material.extensions.fragDepth = true;
+
+        return material;
+    }
+
+}
