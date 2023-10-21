6fa02510d2d6e86d25072ef9abc13d45f72bdf92
Mark Kellogg <mkkellogg@gmail.com>
2023-10-20 21:21:33 -0700

    Enable minified output

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,30 @@
 import { base64 } from "./util/import-base-64.js";
+import terser from '@rollup/plugin-terser';
 
 export default [
     {
         input: './src/index.js',
         treeshake: false,
         external: p => /^three/.test( p ),
-        output: {
-            name: 'Gaussian Splat 3D',
-            extend: true,
-            format: 'umd',
-            file: './build/gaussian-splat-3d.umd.cjs',
-            sourcemap: true,
-            globals: p => /^three/.test( p ) ? 'THREE' : null,
-        },
+        output: [
+            {
+                name: 'Gaussian Splat 3D',
+                extend: true,
+                format: 'umd',
+                file: './build/gaussian-splat-3d.umd.cjs',
+                sourcemap: true,
+                globals: p => /^three/.test( p ) ? 'THREE' : null,
+            },
+            {
+                name: 'Gaussian Splat 3D',
+                extend: true,
+                format: 'umd',
+                file: './build/gaussian-splat-3d.umd.min.cjs',
+                sourcemap: true,
+                globals: p => /^three/.test( p ) ? 'THREE' : null,
+                plugins: [terser()]
+            }
+        ],
         plugins: [
             base64({ include: "**/*.wasm" })
         ]
@@ -21,13 +33,23 @@ export default [
         input: './src/index.js',
         treeshake: false,
         external: p => /^three/.test( p ),
-        output: {
-            name: 'Gaussian Splat 3D',
-            format: 'esm',
-            file: './build/gaussian-splat-3d.module.js',
-            sourcemap: true,
-            globals: p => /^three/.test( p ) ? 'THREE' : null,
-        },
+        output: [
+            {
+                name: 'Gaussian Splat 3D',
+                format: 'esm',
+                file: './build/gaussian-splat-3d.module.js',
+                sourcemap: true,
+                globals: p => /^three/.test( p ) ? 'THREE' : null,
+            },
+            {
+                name: 'Gaussian Splat 3D',
+                format: 'esm',
+                file: './build/gaussian-splat-3d.module.min.js',
+                sourcemap: true,
+                globals: p => /^three/.test( p ) ? 'THREE' : null,
+                plugins: [terser()]
+            }
+        ],
         plugins: [
             base64({ 
                 include: "**/*.wasm",
