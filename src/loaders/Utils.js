993f6e1416db4a5ea0de8ae05650d0a346f929b3
smileluck <12386904+smileluck@users.noreply.github.com>
2024-05-23 15:57:00 +0800

    feat: Filter the path with parameters. Only the path is reserved

--- src/loaders/Utils.js
+++ src/loaders/Utils.js
@@ -1,8 +1,16 @@
 import { SceneFormat } from './SceneFormat.js';
 
 export const sceneFormatFromPath = (path) => {
+    path = getPathFromURL(path);
     if (path.endsWith('.ply')) return SceneFormat.Ply;
     else if (path.endsWith('.splat')) return SceneFormat.Splat;
     else if (path.endsWith('.ksplat')) return SceneFormat.KSplat;
     return null;
 };
+
+
+function getPathFromURL(url) {
+    const regex = /^[^?#]*(?=\?|\#|$)/;
+    const match = regex.exec(url);
+    return match ? match[0] : '';
+}
